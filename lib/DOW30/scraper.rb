aa22a0492bc159c63dac1c23fde857e4f12bc293
Victor Gonzalez <victorggrodriguez@gmail.com>
2018-11-18 00:26:31 -0400

    Find id selector to get attribute values from nodes sets

--- lib/DOW30/scraper.rb
+++ lib/DOW30/scraper.rb
@@ -2,6 +2,17 @@ class Scraper
 
   def self.get_data
     html = Nokogiri::HTML(open("https://www.investing.com/indices/investing.com-united-states-30-components"))
+    table = html.css("tbody")
+    table_row = table.css("tr")
+
+    table_row.collect do |row|
+      id = row.first[1].split("_").last
+      company_name = row.search("a").text
+      stock_price = row.css(".pid-#{id}-last").text.to_f
+      binding.pry
+    end
+
   end
+
 
 end
