60cc275d9423847ff4a57a12cdd5941728f92107
Victor Gonzalez <victorggrodriguez@gmail.com>
2018-11-18 21:00:58 -0400

    Finish scraping data from investing website

--- lib/DOW30/scraper.rb
+++ lib/DOW30/scraper.rb
@@ -3,15 +3,15 @@ class Scraper
   def self.get_data
     html = Nokogiri::HTML(open("https://www.investing.com/indices/investing.com-united-states-30-components"))
     table = html.css("tbody")
-    table_row = table.css("tr")
+    table_row = table.css("tr")[1..29]
 
-    table_row.collect do |row|
-      id = row.first[1].split("_").last
+    table_row.each do |row|
+      id = row.attribute("id").value.split("_").last
+      #id = row.first[1].split("_").last
       company_name = row.search("a").text
       stock_price = row.css(".pid-#{id}-last").text.to_f
-      binding.pry
+      Dowjones.new(company_name, stock_price)
     end
-
   end
 
 
