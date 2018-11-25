ee10bdcf184b5aee6adede4e93ceeeac44a1ca83
Victor Gonzalez <victorggrodriguez@gmail.com>
2018-11-25 13:52:46 -0400

    Testing user input for wrong spell words

--- lib/DOW30/cli.rb
+++ lib/DOW30/cli.rb
@@ -3,10 +3,13 @@ class CLI
   def run
     greeting
     Scraper.get_data
-    display_companies_list
-    binding.pry
+    @companies = display_companies_list
     ask_user_input
-    sanitize_user_input
+    until sanitize_user_input
+      puts 'Error, Check your spelling, Please Try Again:'
+      ask_user_input
+    end
+    display_company_detail
   end
 
   def greeting
@@ -24,12 +27,16 @@ class CLI
 
   def sanitize_user_input
    if @input.gsub(" ", "").match?(/^[A-Za-z]+$/)
-     display_companies_list
+     true
+   elsif @input.match?(/&/)
+     @companies.any? {|name| name == @input }
+   else
+     false
    end
   end
 
   def display_company_detail
-
+  binding.pry
   end
 
 
