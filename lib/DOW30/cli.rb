e5032277bdf9d743ce5e150630d465e55ea08f67
Victor Gonzalez <victorggrodriguez@gmail.com>
2018-11-25 10:53:24 -0400

    Call Scraper.get_data so the data can be display to the user

--- lib/DOW30/cli.rb
+++ lib/DOW30/cli.rb
@@ -2,10 +2,17 @@ class CLI
 
   def run
     greeting
+    #binding.pry
+    Scraper.get_data
+    display_companies_list
   end
 
   def greeting
     puts "Welcome to next Gen Stock Market App!\nThis will show all the companies listed in the Dow Jones 30\n"
+  end
+
+  def display_companies_list
+    Dowjones.company_names_collection.each_with_index { |name, index| puts "#{index + 1}. #{name}" }
   end
 
 
