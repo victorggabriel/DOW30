20145d8bb6d2eaf9beffe72349f20a3a37939732
Victor Gonzalez <victorggrodriguez@gmail.com>
2018-11-25 09:33:39 -0400

    Create company collection class method & test method

--- lib/DOW30/dowjones.rb
+++ lib/DOW30/dowjones.rb
@@ -13,7 +13,25 @@ class Dowjones
   end
 
   def self.all
-    p @@all
+     @@all
+  end
+
+  def self.find_by_index(index)
+     self.all[index]
+  end
+
+  def self.company_names_collection
+    self.all.collect do |dow_obj|
+      dow_obj.company_name
+    end
+  end
+
+  def self.find_by_name(company)
+    self.all.each do |key|
+      if key.company_name.upcase == company.upcase
+        puts "#{key.company_name} is trading at #{key.stock_price}"
+      end
+    end
   end
 
 
