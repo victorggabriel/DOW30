1729d4096666b532ea21047507b9e7bdeb3f1b68
Victor Gonzalez <victorggrodriguez@gmail.com>
2018-11-25 11:41:52 -0400

    Add user input method

--- lib/DOW30/cli.rb
+++ lib/DOW30/cli.rb
@@ -5,17 +5,21 @@ class CLI
     #binding.pry
     Scraper.get_data
     display_companies_list
+    ask_user_input
   end
 
   def greeting
-    puts "Welcome to next Gen Stock Market App!\nThis will show all the companies listed in the Dow Jones 30\n"
+    puts "Welcome to next Gen Stock Market App!\nThis will show all the companies listed in the Dow Jones 30\n\n"
   end
 
   def display_companies_list
     Dowjones.company_names_collection.each { |name| puts "#{name}" }
   end
 
-
+  def ask_user_input
+    puts "\nSelect Company from the list above\nEnter company name: "
+    @input = gets.strip
+  end
 
 
 end
