1939b3df4947b65b76c3d49fcb067eec70108a0f
Victor Gonzalez <victorggrodriguez@gmail.com>
2018-11-25 10:12:15 -0400

    Build CLI class, greeting method & run cli in executable file

---
+++ lib/DOW30/cli.rb
@@ -0,0 +1,13 @@
+class CLI
+
+  def run
+    greeting
+  end
+
+  def greeting
+    puts "Welcome to next Gen Stock Market App!\nThis will show all the companies listed in the Dow Jones 30\n"
+  end
+
+
+
+end
